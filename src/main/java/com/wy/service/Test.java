ec08e883592cdee1e05e5b1a21945c666f25818e
wangyong5609 <1137917270@qq.com>
2023-05-20 20:48:42 +0800

    P11 手写模拟Spring AOP机制

--- src/main/java/com/wy/service/Test.java
+++ src/main/java/com/wy/service/Test.java
@@ -8,7 +8,7 @@ public class Test {
         ApplicationContext applicationContext = new ApplicationContext(AppConfig.class);
         UserService userService = (UserService) applicationContext.getBean("userService");
         userService.test();
-
+        // 这里必须使用接口接收返回值，因为AOP代理的是OrderInterface
         OrderInterface orderService = (OrderInterface) applicationContext.getBean("orderService");
         orderService.test();
     }
